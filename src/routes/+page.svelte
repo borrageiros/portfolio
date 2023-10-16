69dde19aff675641af0816eec506df109f8550ff
Adrián Borrageiros Mourelos <adrian.borrageiros@netexlearning.com>
2023-10-16 12:18:14 +0200

    Fix years old script - Home path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,22 @@
+<script>
+    let birthDate = new Date("1999/04/26");
+
+    function getAge(date) {
+        const today = new Date();
+        let age = today.getFullYear() - date.getFullYear();
+        const m = today.getMonth() - date.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < date.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+    $: age = getAge(birthDate);
+</script>
+
 <div id="content">
     <div class="info">
-        <h1 class="name">Adrián Borrageiros Mourelos, <span id="years"></span>  years old</h1>
+        <h1 class="name">Adrián Borrageiros Mourelos, {age} years old</h1>
         <h2 class="who">Full stack developer from Spain</h2>
-        <script> 
-            function getAge(dateString) {
-                var today = new Date();
-                var birthDate = new Date(dateString);
-                var age = today.getFullYear() - birthDate.getFullYear();
-                var m = today.getMonth() - birthDate.getMonth();
-                if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-                    age--;
-                }
-                return age;
-            }
-            var years = document.getElementById('years');
-            years.innerHTML = getAge("1999/04/26");
-        </script>
     </div>
 </div>
